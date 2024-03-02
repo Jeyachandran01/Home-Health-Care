aabfb6ca07f9bdf01b2b8652058d9af96e36d6b6
Jeyachandran S <jeyachandran@crayond.co>
2024-03-02 15:41:09 +0530

    Initial commit

--- apps/react-vite-web/src/pages/login/style.js
+++ apps/react-vite-web/src/pages/login/style.js
@@ -1,9 +1,12 @@
-import OrganizationSignUp from '@assets/organizationSignUp.svg';
+import Background from '@assets/background.jpg';
 
 export const login_style = {
   rootSx: {
-    backgroundImage: `url(${OrganizationSignUp})`,
+    backgroundImage: `url(${Background})`,
     height: '100vh',
     width: '100%',
+    backgroundRepeat: 'no-repeat',
+    backgroundAttachment: 'fixed',
+    backgroundSize: '100% 100%',
   },
 };
